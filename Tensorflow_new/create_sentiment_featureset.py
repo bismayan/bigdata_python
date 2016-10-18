d835fe55319490ac820824738d91d96cb38bd4f4
bismayan <bismayan@gmail.com>
2016-10-18 14:44:45 -0400

    Tensorflow first few programs added

---
+++ Tensorflow_new/create_sentiment_featureset.py
@@ -0,0 +1,11 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+import random, pickle
+from collections import Counter
+
+lemmatizer=WordNetLemmatizer()
+hm_lines=100000000
+
+
